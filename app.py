54eb4b14213bdaa24eba8d49769504a39d02965d
klaudiaandrzejewska <klaudiaandrzejewska999@gmail.com>
2022-04-27 12:22:33 +0000

    xx

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from distutils.log import info
 from flask import Flask, render_template
+
 
 app=Flask(__name__)
 
@@ -10,6 +12,70 @@ def index():
 @app.route('/xd')
 def xd():
     return render_template("xd.html")
+
+@app.route('/flaga_dla_ukrainy')
+def flaga_dla_ukrainy():
+    return render_template("flaga_dla_ukrainy.html")
+
+
+import random
+from moje_programy.character_wiki import character
+from moje_programy.open_poem import open_poem
+@app.route('/brudnopis')
+def brudnopis():
+    super_heroes = ['Bruce Lee', 'Kubuś Puchatek', 'Kopernik', 'Małysz']
+    chosen_hero = random.choice( super_heroes)
+    description = character( chosen_hero).encode('utf-8').decode()
+    poem_lines = open_poem()
+    return render_template("brudnopis.html", hero=chosen_hero, description=description, poem_lines=poem_lines)
+@app.route('/ciekawe-postacie')
+def ciekawe_postacie():
+    lista_ciekawych_postaci = [
+        'Pudzianowski',         # 0
+        'Małysz',               # 1
+        'Kopernik',             # 2
+        'Maria Skłodowska',     # 3
+        'Kościuszko',           # 4
+        'Donald',               # 5
+        'Myszka Miki',          # 6
+    ]
+    opisy_postaci = []
+    for i in range(3):
+        postac = random.choice(lista_ciekawych_postaci)
+        indeks = lista_ciekawych_postaci.index(postac)
+        lista_ciekawych_postaci.pop(indeks)
+
+        opis_postaci = character(postac)
+        dlugosc_opisu = len(opis_postaci.split())
+        info = [postac, opis_postaci, dlugosc_opisu]
+        opisy_postaci.append(info)
+        opisy_postaci.sort(key = lambda x: x[2], reverse = True)
+
+    return render_template("ciekawe-postacie.html", opisy_postaci=opisy_postaci)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__=="__main__":
     app.run()
